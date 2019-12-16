f91902ea74b82085e6d9f962401e75f044b3e9f6
RudzC <rudy.design9@gmail.com>
2019-12-16 16:11:37 -0500

    Step_6.html

---
+++ Exercises/Javascript-Basics-01/Step_6.js
@@ -0,0 +1,6 @@
+function myFunc() {
+  var number1 = document.getElementById("first_number").value;
+  var number2 = document.getElementById("second_number").value;
+  var result = number1 % number2;
+  alert(result);
+}

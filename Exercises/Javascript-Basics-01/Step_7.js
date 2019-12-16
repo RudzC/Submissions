8f443f9bd92eb81febea6e26785e8ea62b801c94
RudzC <rudy.design9@gmail.com>
2019-12-16 16:26:36 -0500

    Step_7.html

---
+++ Exercises/Javascript-Basics-01/Step_7.js
@@ -0,0 +1,6 @@
+function myFunc() {
+  var number1 = document.getElementById("shoe_size").value;
+  var number2 = document.getElementById("year").value;
+  var result = number1 + 5 * 50 - number2 - 1766;
+  alert(result);
+}

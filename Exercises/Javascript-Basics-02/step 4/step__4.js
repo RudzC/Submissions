d00f15f9bba8b6551a94fc3b3f42e66881110655
RudzC <rudy.design9@gmail.com>
2019-12-17 14:57:04 -0500

    step 5

---
+++ Exercises/Javascript-Basics-02/step 4/step__4.js
@@ -0,0 +1,7 @@
+function myFunction() {
+  if (confirm("Yes/No")) {
+    document.getElementById("name").value = "";
+    document.getElementById("surname").value = "";
+    document.getElementById("city").value = "";
+  }
+}
---
+++ Exercises/Javascript-Basics-02/step 5/step__5.js
@@ -0,0 +1,6 @@
+const images = document.querySelectorAll("img");
+for (let i = 0; i < images.length; i++) {
+  images[i].addEventListener("mouseover", () => {
+    images[i].src = "images/image" + (i + 1) + "_2.jpg";
+  });
+}

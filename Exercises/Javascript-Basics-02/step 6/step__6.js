a627564ab28a5c140daebc1e20847a934958c8b4
RudzC <rudy.design9@gmail.com>
2019-12-18 15:35:59 -0500

    step 6

--- Exercises/Javascript-Basics-02/step 6/step__6.js
+++ Exercises/Javascript-Basics-02/step 6/step__6.js
@@ -7,3 +7,13 @@ function myFunc() {
   }
 }
 myFunc();
+
+function funcOut() {
+  const images = document.querySelectorAll("img");
+  for (let i = 0; i < images.length; i++) {
+    images[i].addEventListener("mouseout", () => {
+      images[i].src = "images/image" + (i + 1) + ".jpg";
+    });
+  }
+}
+funcOut();

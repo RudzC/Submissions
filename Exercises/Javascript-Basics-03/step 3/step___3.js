973990b0806b6123f00a50e6d9b1b0024fd99a81
RudzC <rudy.design9@gmail.com>
2019-12-18 16:39:04 -0500

    step 3

---
+++ Exercises/Javascript-Basics-03/step 3/step___3.js
@@ -0,0 +1,9 @@
+document.querySelector(".green").onclick = function() {
+  document.querySelector("p").style.color = "green";
+};
+document.querySelector(".red").onclick = function() {
+  document.querySelector("p").style.color = "red";
+};
+document.querySelector(".blue").onclick = function() {
+  document.querySelector("p").style.color = "blue";
+};

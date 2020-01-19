22a1d1949167a58aec3d2e133fc60986e2618abf
RudzC <rudy.design9@gmail.com>
2020-01-18 22:56:53 -0500

    joins.sh

--- Exercises/SQLite Basic/answers.sh
+++ Exercises/SQLite Basic/answers.sh
@@ -3,26 +3,26 @@ problem0: "select * from students"
 Basic Queries :
 FROM students;
 
-1.
+1.Get all the names of students in the database
 SELECT name
 FROM students;
 
-2.
+2.Get all the data of students above 30 years old
 SELECT *
 FROM students
 WHERE Age > 30
 
-3.
+3.Get the names of the females who are 30 years old
 SELECT name
 FROM students
 WHERE Age = 30 AND Gender = 'F'
 
-4.
+4.Get the number of Points of Alex
 SELECT Points
 FROM students
 WHERE name = 'Alex'
 
-5.
+5.Add yourself as a new student (your name, your age...)
 INSERT INTO students 
 (ID,
 Name, 
@@ -31,16 +31,17 @@ Gender,
 Points)
 VALUES  (8,'Rudy', 32, 'M', 600)
 
-6.
+6.Increase the points of Basma because she solved a new exercise 
 UPDATE students
 set	Points = points + 100
 WHERE name = 'Basma'
 
-7.
+7.Decrease the points of Alex because he is late today
 UPDATE students
 set	Points = points - 30
 WHERE name = 'Alex'
 
+Commit ("Basic Queries") 
 
 CREATE TABLE graduates (
 ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
@@ -50,17 +51,43 @@ Gender TEXT,
 Points INTEGER,
 Graduation TEXT date)
 
-8.
+
+8.Copy Layals data from students to graduates
 INSERT INTO graduates (name, Age, Gender, Points)
 SELECT name, Age, Gender, Points 
 FROM students WHERE name = 'Layal'
 
-9.
+9.Add the graduation date previously mentioned to Layals record in graduates
 UPDATE graduates 
 SET Graduation = '08/09/2018'
 WHERE ID = 1
 
-10.
+10.Remove Layals record from students
 DELETE FROM students
 WHERE id = 4
 
+Commit ("Creating Table") 
+
+11.Produce a table that contains, for each employee, his/her name, company name, and company date.
+CREATE TABLE Joins (
+Name TEXT,
+Company TEXT,
+Date TEXT date)
+
+12.
+INSERT INTO Joins (name, Company, date)
+SELECT employees.name, companies.name, companies.date
+FROM employees join companies
+ON companies.name = employees.Company
+
+13.Find the name of employees that work in companies made before 2000.
+SELECT name FROM joins
+WHERE date < 2000
+
+14.Find the name of company that has a graphic designer.
+SELECT Company FROM employees
+WHERE role = "Graphic Designer"
+
+Commit ("Joins")
+
+15.

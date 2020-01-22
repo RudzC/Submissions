ef0ae14bd28fbe995348e87f8201187a0c31b40d
RudzC <rudy.design9@gmail.com>
2020-01-22 16:20:25 -0500

    step 4

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -22,6 +22,28 @@ app.get("/time", (req, res) => {
   var m = d.getMinutes();
   res.json({ status: 200, message: n + ":" + m });
 });
+
+app.get("/hello/:id?", (req, res) => {
+  const id = req.params.id;
+  if (id) {
+    res.json({ status: 200, message: "hello " + id });
+  } else res.json({ status: 200, message: "hello" });
+});
+app.get("/search", (req, res) => {
+  const s = req.query.s;
+  if (s) {
+    res.json({ status: 200, message: "ok", data: s });
+  } else {
+    res
+      .status(500)
+      .json({
+        status: 500,
+        error: true,
+        message: "you have to provide a search!"
+      });
+  }
+});
+
 // make the server listen to requests
 app.listen(PORT, () => {
   console.log(`Server running at: http://localhost:${PORT}/`);
